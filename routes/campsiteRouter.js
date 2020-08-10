339dcabdf865c5aa9cdb03100c0d84317d06474d
MarquesDragan <47039402+MarquesDragan@users.noreply.github.com>
2020-08-10 14:39:10 -0500

    Week 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the selected ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.campsiteId} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
